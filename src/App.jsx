28de0d211f0d4271e10a5f1701ceb887bbffd05b
Shammi Nethupul <132045629+cycotechnolgies@users.noreply.github.com>
2024-07-15 21:55:23 +0530

    Add Tailwind Css Library

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TestComponent() {
+  return (
+    <>
+      <h1 className="font-bold text-3xl underline flex justify-center items-center">
+        Hello World...!
+      </h1>
+    </>
+  );
+}
+
+export default TestComponent;
