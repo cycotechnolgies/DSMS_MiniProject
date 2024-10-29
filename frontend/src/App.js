7d7e29f9e838a64805b4eee80e37a1929bc23273
Shammi Nethupul <132045629+cycotechnolgies@users.noreply.github.com>
2024-10-29 16:40:22 +0530

    add tailwind to frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+    return<>
+        <h1 className="text-4xl font-bold text-blue-600">Hello World</h1>
+    </>;
+}
+
+export default App;
