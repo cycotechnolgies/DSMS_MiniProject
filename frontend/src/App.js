d16a956c758af766d170dd89584f32639e39adac
Shammi Nethupul <132045629+cycotechnolgies@users.noreply.github.com>
2024-11-01 10:32:57 +0530

    configure frontend with shadCN

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-function App() {
-    return<>
-        <h1 className="text-4xl font-bold text-blue-600">Hello World</h1>
-    </>;
-}
-
-export default App;
