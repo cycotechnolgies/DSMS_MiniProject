d7e4e391db10f85a51396fa9ab4349884e9ff0d1
Shammi Nethupul <132045629+cycotechnolgies@users.noreply.github.com>
2024-11-01 18:22:30 +0530

    login page frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+// import { Button } from "./components/ui/button";
+import { LoginForm } from "./components/ui/Login";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
     <>
-      <Button>Fuck</Button>
+      <LoginForm></LoginForm>
+      <Toaster></Toaster>
     </>
   );
 }
